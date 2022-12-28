449085a1f027c79362cfd6efb0f54d5e064843e1
PNKlethal <seregaa99@bk,ru>
2022-12-28 14:21:46 +0600

    -

--- main_1.py
+++ main_1.py
@@ -16,7 +16,7 @@ print(timeit("my_type",globals=globals() , number=10000))
 "0.00010750000365078449"
 
 list = [1, 2, -30, 'string', True, 3.5]
-
+"""Здесь я просто последовал вашему замечанию про то, что бесмысленно усложнил код, после правок, время уменьшилось"""
 def my_type_optimizmed(el):
     for el in list:
         print(type(list))
